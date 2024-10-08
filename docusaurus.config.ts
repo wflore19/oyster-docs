d8765ffa08ab9ee039a80a753668656a4eda0e30
wflore19 <wflore19@montgomerycollege.edu>
2024-10-08 06:46:52 -0400

    add: gh pages deployment config

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -8,17 +8,18 @@ const config: Config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://wflore19.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/oyster-docs/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'wflore19', // Usually your GitHub org/user name.
   projectName: 'oyster-docs', // Usually your repo name.
+  deploymentBranch: 'gh-pages',
   trailingSlash: false,
-  
+
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
 
